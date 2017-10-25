9229cc17efac7ea9bc5f0afb1b6382a0575f0170
mbodenhamer <mbodenhamer@mbodenhamer.com>
2017-10-25 16:55:54 -0500

    Adding tests

---
+++ test.py
@@ -0,0 +1,25 @@
+import os
+from yatr import Document
+
+DIR = os.path.abspath(os.path.dirname(__file__))
+
+#-------------------------------------------------------------------------------
+
+def find_yamlfiles(path):
+    for dirpath, _, fnames in os.walk(path):
+        for fname in fnames:
+            if fname.endswith('.yml'):
+                yield os.path.join(dirpath, fname)
+
+#-------------------------------------------------------------------------------
+
+def test():
+    for fpath in find_yamlfiles(os.path.join(DIR, 'yatrfiles')):
+        doc = Document.from_path(fpath)
+        doc.env.resolve_macros()
+        print(fpath) # If we get here, it validated
+
+#-------------------------------------------------------------------------------
+
+if __name__ == '__main__':
+    test()
