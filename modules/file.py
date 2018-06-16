12f252ab671c493bae2dfbb52d412e340af612a4
mbodenhamer <mbodenhamer@mbodenhamer.com>
2018-06-15 23:32:19 -0500

    Adding modules/file.py

---
+++ modules/file.py
@@ -0,0 +1,25 @@
+'''Yatr extension module for file operations.'''
+
+from yatr import Env
+env = Env()
+
+#-------------------------------------------------------------------------------
+# replace_in_file
+
+@env.task('replace_in_file', display=('path', 'pattern', 'repl'))
+def replace_in_file(env, *args, **kwargs):
+    import re
+
+    path = kwargs['path']
+    pattern = kwargs['pattern']
+    repl = kwargs['repl']
+
+    with open(path, 'r') as f:
+        txt = f.read()
+
+    re.sub(pattern, repl, txt)
+
+    with open(path, 'w') as f:
+        f.write(txt)
+
+#-------------------------------------------------------------------------------
